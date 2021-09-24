49adca8d0a787d393ae3762a882d30eabbc96493
Daxidxad <91004264+Daxidxad@users.noreply.github.com>
2021-09-24 00:12:15 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,27 @@
-let Luz = 0
-let Critico = false
-let Alta_Temp = false
-let Estable = false
-let Baja_Temp = false
-let Temp = 0
+// Inicio
+// 
+// 
 radio.setGroup(1)
-pins.analogWritePin(AnalogPin.P1, 0)
+pins.analogWritePin(AnalogPin.P1, 1023)
 pins.analogWritePin(AnalogPin.P2, 0)
+let Temp = input.temperature()
+let Luz = input.lightLevel()
 let Humedad = pins.analogReadPin(AnalogPin.P0)
+let Baja_Temp = Temp >= 10 && Temp <= 16
+let Estable = Temp >= 20 && Temp <= 27
+let Alta_Temp = Temp >= 28 && Temp <= 34
+let Critico = Humedad >= 600 && Temp >= 35
 basic.showString("L")
 basic.pause(100)
 led.plotBarGraph(
 input.lightLevel(),
 395
 )
-basic.pause(5000)
+basic.pause(1000)
 basic.showString("T")
 led.plotBarGraph(
 Temp,
-40
+36
 )
 basic.pause(2000)
 basic.showString("H")
@@ -26,24 +29,28 @@ led.plotBarGraph(
 Humedad,
 200
 )
-basic.pause(5000)
+basic.pause(1000)
+// Controles
+// 
 basic.forever(function () {
-    Baja_Temp = Temp >= 10 && Temp <= 16
-    Estable = Temp >= 20 && Temp <= 27
-    Alta_Temp = Temp >= 28 && Temp <= 34
-    Critico = Humedad >= 600 && Temp >= 35
-})
-basic.forever(function () {
+    basic.pause(1000)
+    led.plotBarGraph(
+    Humedad,
+    200
+    )
+    basic.pause(1000)
     if (Temp >= 20 && Temp <= 35) {
+        basic.showNumber(Temp)
         led.plotBarGraph(
         Temp,
-        140
+        20
         )
-        basic.pause(5000)
+        basic.pause(1000)
         basic.showString("ESTABLE")
     }
-    basic.pause(5000)
+    basic.pause(1000)
     if (Temp >= 35 && Temp <= 34) {
+        basic.showNumber(Temp)
         led.plotBarGraph(
         Temp,
         45
@@ -53,11 +60,12 @@ basic.forever(function () {
     }
     basic.pause(1000)
     if (Temp >= 10 && Temp <= 16) {
+        basic.showNumber(Temp)
         led.plotBarGraph(
         Temp,
         150
         )
-        basic.pause(2000)
+        basic.pause(1000)
         basic.showString("BAJA.TEMP")
     }
     if (Humedad >= 300 && Temp >= 35) {
@@ -76,9 +84,42 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
-    Temp = input.temperature()
-    Humedad = pins.analogReadPin(AnalogPin.P0)
-    Luz = input.lightLevel()
+    while (Humedad <= 300) {
+        pins.analogWritePin(AnalogPin.P2, 1023)
+    }
+    basic.pause(100)
+    while (pins.analogReadPin(AnalogPin.P0) <= 300) {
+        pins.analogWritePin(AnalogPin.P2, 1023)
+    }
+    basic.pause(100)
+})
+// Transmicion
+// 
+basic.forever(function () {
+    if (Temp >= 10 && Temp <= 16) {
+        radio.sendString("Baja.Temp")
+        basic.pause(500)
+        radio.sendString("" + (Baja_Temp))
+    }
+    basic.pause(500)
+    if (Temp >= 20 && Temp <= 27) {
+        radio.sendString("Estable")
+        basic.pause(500)
+        radio.sendString("" + (Estable))
+    }
+    basic.pause(500)
+    if (Temp >= 28 && Temp <= 34) {
+        radio.sendString("Alta.Temp")
+        basic.pause(500)
+        radio.sendString("" + (Alta_Temp))
+    }
+    basic.pause(500)
+    if (Humedad >= 600 && Temp >= 35) {
+        radio.sendString("Critico")
+        basic.pause(500)
+        radio.sendString("" + (Critico))
+    }
+    basic.pause(500)
 })
 basic.forever(function () {
     basic.pause(1000)
@@ -97,42 +138,11 @@ basic.forever(function () {
     }
     basic.pause(1000)
     if (input.buttonIsPressed(Button.AB)) {
+        basic.pause(1000)
         led.plotBarGraph(
         Luz,
         395
         )
     }
     basic.pause(1000)
-})
-basic.forever(function () {
-    if (Humedad < 300) {
-        pins.analogWritePin(AnalogPin.P1, 1023)
-    } else {
-        pins.analogWritePin(AnalogPin.P1, 0)
-    }
-    basic.pause(100)
-    if (Luz < 100) {
-        pins.analogWritePin(AnalogPin.P2, 1023)
-    } else {
-        pins.analogWritePin(AnalogPin.P2, 0)
-    }
-    basic.pause(100)
-})
-control.inBackground(function () {
-    if (Temp >= 10 && Temp <= 16) {
-        radio.sendString("Baja.Temp")
-    }
-    basic.pause(500)
-    if (Temp >= 20 && Temp <= 27) {
-        radio.sendString("Estable")
-    }
-    basic.pause(500)
-    if (Temp >= 28 && Temp <= 34) {
-        radio.sendString("Alta.Temp")
-    }
-    basic.pause(500)
-    if (Humedad >= 600 && Temp >= 35) {
-        radio.sendString("Critico")
-    }
-    basic.pause(500)
 })
